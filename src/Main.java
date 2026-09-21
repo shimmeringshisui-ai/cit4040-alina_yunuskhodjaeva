fc6356410fd366f0d18bc89269fcc6f978e80e00
shimmeringshisui <shimmeringshisui@gmail.com>
2026-09-21 10:12:50 +0600

    Add the Hello program that prints my name

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alina";
+        int a = 12;
+        int b = 30;
+
+        System.out.println("Hi, " + name);
+        System.out.println("a + b = " + (a + b));
+    }
+}
